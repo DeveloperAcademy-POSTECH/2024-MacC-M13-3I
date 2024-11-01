e956ca601c8588a67aa40179efe3d1a936798f34
SSSiye <tldp6036@naver.com>
2024-11-01 11:41:13 +0900

    Initial Commit

---
+++ PennyPack/PennyPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  PennyPackApp.swift
+//  PennyPack
+//
+//  Created by siye on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PennyPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
