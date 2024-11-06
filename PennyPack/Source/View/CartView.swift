536599b6abfa085f67d94c79259a85a0ea7dcbfa
Winterrr-24 <dbwls9924@naver.com>
2024-11-06 09:23:53 +0900

    [Feat]#6 - 정규표현식 연습

--- PennyPack/Source/View/CartView.swift
+++ PennyPack/Source/View/CartView.swift
@@ -8,36 +8,64 @@
 import Foundation
 import SwiftUI
 
+//struct DateItem: Codable, Hashable {
+//    var date: Date
+//    var items: [ShoppingItem]
+//    var total: Int //총 금액
+//    var place: String
+//}
+
+struct ShoppingItem: Identifiable, Codable, Hashable {
+    var id = UUID()
+    var name: String
+//    var quantity: Int
+//    var unitPrice: Int //단가
+//    var price: Int //수량*단가
+//    var time: Date
+}
+
+//    @Published var shoppingItem: [ShoppingItem] = []
+    
 struct CartView: View {
-    @State var recognizedText = ["씨씨", "윈터", "이지"]
+//    @ObservedObject var shoppingViewModel: ShoppingViewModel
+    @State var shoppingItems: [ShoppingItem] = []
+    @Binding var recognizedText: String
+    @State var items: [String] = []
     @State private var multiSelection = Set<UUID>()
     
     var body: some View {
-        NavigationView {
-            List(selection: $multiSelection) {
-                ForEach(recognizedText, id: \.self) { recognizedText in Text(recognizedText)
-                }
-                .onMove(perform: move)
-                .onDelete(perform: delete)
-            }
-            .navigationBarItems(trailing: EditButton())
-        }
+//        NavigationView {
+//            List(selection: $multiSelection) {
+//                ForEach(recognizedText, id: \.self) { recognizedText in Text(recognizedText)
+//                }
+//                .onMove(perform: move)
+//                .onDelete(perform: delete)
+//            }
+//            .navigationBarItems(trailing: EditButton())
+//        }
+//        Text(recognizedText)
     }
     
-    func move(from source: IndexSet, to destination: Int) {
-        let reversedSource = source.sorted()
-        
-        for index in reversedSource.reversed() {
-            recognizedText.insert(recognizedText.remove(at: index), at: destination)
-        }
+    func addList(_ text: String) {
+        let newItem = ShoppingItem(name: text)
+                shoppingItems.append(newItem)
+        items.append(recognizedText)
     }
-    func delete(at offsets: IndexSet) {
-        if let first = offsets.first {
-            recognizedText.remove(at: first)
-        }
-    }
+    
+//    func move(from source: IndexSet, to destination: Int) {
+//        let reversedSource = source.sorted()
+//        
+//        for index in reversedSource.reversed() {
+//            recognizedText.insert(recognizedText.remove(at: index), at: destination)
+//        }
+//    }
+//    func delete(at offsets: IndexSet) {
+//        if let first = offsets.first {
+//            recognizedText.remove(at: first)
+//        }
+//    }
 }
 
-#Preview {
-    CartView()
-}
+//#Preview {
+//      CartView(recognizedText: $recognizedText)
+//}
--- PennyPack/Source/View/ScanView.swift
+++ PennyPack/Source/View/ScanView.swift
@@ -11,13 +11,16 @@ import Vision
 
 struct ScanView: View {
     @State private var recognizedText = ""
+    @State private var recognizedTexts: [String] = []
 
     var body: some View {
         VStack {
             DocumentScannerView(recognizedText: $recognizedText)
+                .frame(width: 353, height: 300)
             ScrollView {
                 Text(recognizedText)
-            }
+            }.frame(height: 100)
+            CartView(recognizedText: $recognizedText)
         }
     }
 }
@@ -26,34 +29,8 @@ struct ScanView: View {
     ScanView()
 }
 
-//struct ContentsView: View {
-//    @State private var recognizedText = ""
-//    @State private var isShowDocumentScanner = false
-//
-//    var body: some View {
-//        VStack {
-//            ScrollView {
-//                Text(recognizedText)
-//            }
-//
-//            Button("Scan Documents") {
-//                isShowDocumentScanner = true
-//                recognizedText = ""
-//            }
-//            .sheet(isPresented: $isShowDocumentScanner) {
-//                DocumentScannerView(recognizedText: $recognizedText)
-//            }
-//        }
-//        .padding()
-//    }
-//}
-//
-//#Preview {
-//    ContentsView()
-//}
 
-
-
+// UIViewControllerRepresentable
 struct DocumentScannerView: UIViewControllerRepresentable {
     @Binding var recognizedText: String
     
@@ -70,6 +47,7 @@ struct DocumentScannerView: UIViewControllerRepresentable {
     }
 }
 
+// VNRecognizeTextRequest
 class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
     var parent: DocumentScannerView
     
@@ -101,6 +79,7 @@ class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
             }
         }
         
+//        request.regionOfInterest = CGRect(x: 0.25, y: 0.25, width: 0.5, height: 0.5)
         request.recognitionLanguages = ["Ko"]
         request.usesLanguageCorrection = true
         
@@ -124,3 +103,49 @@ struct DocumentScannerView_Previews: PreviewProvider {
 //#Preview {
 //    DocumentScannerView_Previews() as! any View
 //}
+
+
+import NaturalLanguage
+
+let textstring = "Knowledge will give you power"
+let tagger = NLTagger(tagSchemes: [.language, .lexicalClass])
+
+
+
+//import Foundation
+//func pricing(_ text: String) -> String {
+//    let pattern = #"^\d{1,2}\.\d{1,2}$"#
+//    let regex = try! NSRegularExpression(pattern: pattern, options: [])
+//        let range = NSRange(location: 0, length: text.utf16.count)
+//        let cleanedText = regex.stringByReplacingMatches(in: text, options: [], range: range, withTemplate: "")
+//        return cleanedText.trimmingCharacters(in: .whitespacesAndNewlines)
+//}
+//    let prices = ["4.33", "4€ .33", "14.33"]
+////    let predicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+////    let validPrices = prices.filter { predicate.evaluate(with: $0) }
+////    print(validPrices)
+////}
+//
+////for string in validPrices {
+////    guard let range = string.range(of: pattern, options: .regularExpression) else {
+////        print("\(string)에는 숫자가 없습니다.")
+////        continue
+////    }
+////    print("\(string)에서 숫자로 이루어 진 부분은 \(string[range]) 입니다.") // 가장 앞에 일치하는 부분만 확인 가능
+////}
+//
+////func LO() {
+////    let price = "4.33"
+////    let regularExpression = #"^\d{1,2}\.\d{1,2}[^€/kg]$"#
+////    
+////    if let targetIndex = price.range(of: regularExpression, options: .regularExpression) {
+////        print("정규식과 일치")
+////        print("휴대폰번호 - \(price[targetIndex])")
+////    } else {
+////        print("정규식과 불일치")
+////    }
+////}
+////print(LO("12.98"))
+//
+//
+//
